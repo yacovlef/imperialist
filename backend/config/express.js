6266c35f870c5988f3b020ac3a36871cc940c72c
yakovlev <yakovlev@tbm.ru>
2019-11-18 10:51:04 +0300

    18.11

--- backend/config/express.js
+++ backend/config/express.js
@@ -1,7 +1,16 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: path.join(__dirname, '..', 'log')
+});
 
 module.exports = (app) => {
     app.use(bodyParser.json());
     app.use(cors());
-}
+    app.use(morgan('combined', { stream: accessLogStream }));
+};
