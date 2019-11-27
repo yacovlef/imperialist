8755281e8b22b893d5b2fba8555ca6998ed9f811
yakovlev <yakovlev@tbm.ru>
2019-11-27 08:42:36 +0300

    27.11

--- frontend/src/components/Project/ProjectListItem/ProjectListItem.jsx
+++ frontend/src/components/Project/ProjectListItem/ProjectListItem.jsx
@@ -28,22 +28,23 @@ const ProjectListItem = ({ projectItem }) => {
                 <div>Создан: {moment(createdAt).format('DD-MM-YYYY HH:mm')}</div>
             </div>
 
-            <div>
-                <div className="project_list_item__material_header">
-                    <div>#</div>
-                    <div>Наименование</div>
-                    <div>Кол-во</div>
-                    <div>Ед. изм.</div>
-                    <div>Цена за ед.</div>
-                    <div>Цена итог.</div>
-                    <div></div>
-                </div>
+            {(!!Materials.length) &&
+                <div>
+                    <div className="project_list_item__material_header">
+                        <div>#</div>
+                        <div>Наименование</div>
+                        <div>Кол-во</div>
+                        <div>Ед. изм.</div>
+                        <div>Цена за ед.</div>
+                        <div>Цена итог.</div>
+                        <div></div>
+                    </div>
 
-                <div className="project_list_item__material_item">
-                    { Materials.map((material, index) => <ProjectListItemMaterial material={material} index={index} key={index} />) }
+                    <div className="project_list_item__material_item">
+                        { Materials.map((material, index) => <ProjectListItemMaterial material={material} index={index} key={index} />) }
+                    </div>
                 </div>
-
-            </div>
+            }
 
             <div className="project_list_item__action">
                 <div>
